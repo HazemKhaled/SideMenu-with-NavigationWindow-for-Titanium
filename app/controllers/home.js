6b78b5a3dae9a51de189ed81be0b904efb260581
Hazem Khaled <hazem.khaled@gmail.com>
2015-12-27 19:40:13 +0200

    Add example menu items in both home and page 1

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -18,5 +18,33 @@ if (OS_IOS) {
 	navButton.addEventListener('click', toggle);
 
 	$.home[direction + 'NavButton'] = navButton;
-
 }
+
+$.home.onCreateOptionsMenu = function(e) {
+
+	// Clean up all items from any other controller
+	e.menu.clear();
+
+	// Add our items
+	var menuItem = e.menu.add({
+		title: "Add Task"
+	});
+	menuItem.addEventListener("click", function() {
+		alert("Let's add it");
+	});
+
+	// SearchView
+	var searchView = Ti.UI.Android.createSearchView({
+		hintText: "Search here ..."
+	});
+
+	searchView.addEventListener('submit', function(e) {
+		alert('Search for: ' + e.source.value);
+	});
+
+	e.menu.add({
+		title: 'Search',
+		showAsAction: Ti.Android.SHOW_AS_ACTION_ALWAYS,
+		actionView: searchView
+	});
+};
--- app/controllers/page1.js
+++ app/controllers/page1.js
@@ -1,3 +1,17 @@
 function openPage1() {
 	Alloy.Globals.windowStack.open(Alloy.createController('page').getView());
 }
+
+$.page1.onCreateOptionsMenu = function(e) {
+	// Clean up all items from any other controller
+	e.menu.clear();
+
+	// Add our items
+	var menuItem = e.menu.add({
+		title: "Button",
+		showAsAction: Ti.Android.SHOW_AS_ACTION_ALWAYS
+	});
+	menuItem.addEventListener("click", function() {
+		alert("Hey another button");
+	});
+};
